853c7d322d02a1a81f6a2c89aae5f62d59c697c7
Oliver Foster <oliver.foster@kineo.com>
2016-07-20 22:13:05 +0100

    Update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ events.on("interact-registry:loaded", function() {
 	FileSystem.mkdir( path.join(cwd, build) );
 
 	cli.plugins.install(path.join(cwd, src), [
-		'interact-registry-frontend-src'
+		'interact-registry-frontend-app'
 	], {
 		success: function() {
 
